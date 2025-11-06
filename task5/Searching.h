e8407120db7807bf7ce92fde500f19593c37aae9
eemreeravci01 <eravcen1388_aehit@students.vizja.pl>
2025-11-07 00:32:09 +0100

    update

--- task5/Searching.h
+++ task5/Searching.h
@@ -1,12 +1,65 @@
 #include <array>
 #include <vector>
+
+template <typename T, unsigned long N>
+int binarySearchFirst(const std::array<T, N> arr, T target) 
+{
+	//write the body for this function
+	//function must return the index of the first occurence for the target element or
+	//must return -1 if target doesnt exists
+	int left = 0, right = N - 1;
+	int result = -1;
+	while (left <= right) 
+	{
+		int middle = left + (right - left) / 2;
+
+		if (arr[middle] == target) {
+			result = middle;   
+			right = middle - 1; 
+		} 
+		else if (arr[middle] > target) {
+			right = middle - 1;
+		} 
+		else {
+			left = middle + 1;
+		}
+	}
+	return result;
+}
+
+template <typename T>
+int binarySearchFirst(const std::vector<T>& v, T target) {
+	//write the body for this function
+	//function must return the index of the first occurence for the target element or
+	//must return -1 if target doesnt exists
+	int left = 0;
+	int right = v.size() - 1;
+	int result = -1;
+
+	while (left <= right) {
+		int middle = left + (right - left) / 2;
+
+		if (v[middle] == target) {
+			result = middle;   
+			right = middle - 1;   
+		}
+		else if (v[middle] > target) {
+			right = middle - 1;   
+		}
+		else {
+			left = middle + 1;    
+		}
+	}
+	return result;
+}
+
 /*
 template <typename T, unsigned long N>
 int linearSearch(std::array<T, N> arr, T target) {
 	for (int i = 0; i < N; i++) {
 		if (arr[i] == target) {
 			return i;
-		}
+			}
 	}
 	return -1;
 }
@@ -82,54 +135,3 @@ int binarySearch(std::vector<T> arr, T target) {
 }
 */
 
-template <typename T, unsigned long N>
-int binarySearchFirst(const std::array<T, N> arr, T target) 
-{
-	//write the body for this function
-	//function must return the index of the first occurence for the target element or
-	//must return -1 if target doesnt exists
-    int left = 0, right = N - 1;
-    int result = -1;
-    while (left <= right) 
-	{
-		int middle = left + (right - left) / 2;
-
-        if (arr[middle] == target) {
-            result = middle;   
-            right = middle - 1; 
-        } 
-        else if (arr[middle] > target) {
-            right = middle - 1;
-        } 
-        else {
-            left = middle + 1;
-        }
-    }
-    return result;
-}
-
-template <typename T>
-int binarySearchFirst(const std::vector<T>& v, T target) {
-	//write the body for this function
-	//function must return the index of the first occurence for the target element or
-	//must return -1 if target doesnt exists
-	int left = 0;
-    int right = v.size() - 1;
-    int result = -1;
-
-    while (left <= right) {
-        int middle = left + (right - left) / 2;
-
-        if (v[middle] == target) {
-            result = middle;   
-			right = middle - 1;   
-        }
-        else if (v[middle] > target) {
-            right = middle - 1;   
-        }
-        else {
-            left = middle + 1;    
-        }
-    }
-    return result;
-}
