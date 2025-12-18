1abccb3328e52c92b0f834fb194c320ec76ac899
eemreeravci01 <eravcen1388_aehit@students.vizja.pl>
2025-12-18 15:53:46 +0100

    task8

--- task8/SortingApp.cpp
+++ task8/SortingApp.cpp
@@ -1,21 +1,58 @@
 #include <iostream>
 #include <string>
 #include "Sorting.h"
+#include "RandomNumber.h"
+#include "Benchmark.h"
 
 int main()
 {
-    std::vector<int> v1={2, 7, 1, 3, 6, 2, 6};
-    std::vector<double> v2={1.5, 2, 6.3, 5.6, 1.1, 0.9};
-    std::vector<std::string> v3= {"enes", "emre", "eravci", "hello", "world", "ale"};
-    for(int i = 0; i<v1.size(); i++)
-    {
-        std::cout<< v1[i] << " ";
-    }
-    std::cout << std::endl;
+    //when i want to change size i am updating manually while using SIZE variable
+    const int SIZE = 50000;
+
+    auto v1 = generateRandomNumbers(SIZE);
+    auto v2 = generateSortedVector(SIZE);
+    auto v3 = generateReverseSortedVector(SIZE);
+    
+    // bubble sort
+    /*
+    Benchmark<> b1;
+    bubleSort(v1, ascending<int>); 
+    std::cout << "Random: " << b1.elapsed() / 1000000.0 << "\n";
+    bubleSort(v2, ascending<int>);
+    std::cout << "Sorted: " << b1.elapsed() / 1000000.0 << "\n";
+    bubleSort(v3, ascending<int>);
+    std::cout << "Reverse Sorted: " << b1.elapsed() / 1000000.0 << "\n";
+    */
+    
+    // bubble sort with flag
+    /*
+    Benchmark<> b2;
+    bubleSortWithFlag(v1, ascending<int>);
+    std::cout << "Random: " << b2.elapsed() / 1000000.0 << "\n";
+    bubleSortWithFlag(v2, ascending<int>);
+    std::cout << "Sorted: " << b2.elapsed() / 1000000.0 << "\n";
+    bubleSortWithFlag(v3, ascending<int>);
+    std::cout << "Reverse Sorted: " << b2.elapsed() / 1000000.0 << "\n"; 
+    */
+
+    // insertion sort
+    /*
+    Benchmark<> b3;
     insertionSort(v1, ascending<int>);
-    for(int i = 0; i < v1.size(); i++)
-    {
-        std::cout << v1[i] << " ";
-    }
-    std::cout << std::endl;
+    std::cout << "Random: " << b3.elapsed() / 1000000.0 << "\n";
+    insertionSort(v2, ascending<int>);
+    std::cout << "Sorted: " << b3.elapsed() / 1000000.0 << "\n";
+    insertionSort(v3, ascending<int>);
+    std::cout << "Reverse Sorted: " << b3.elapsed() / 1000000.0 << "\n"; 
+    */
+    
+    // selection sort
+    Benchmark<> b4;
+    selectionSort(v1, ascending<int>);
+    std::cout << "Random: " << b4.elapsed() / 1000000.0 << "\n";
+    selectionSort(v2, ascending<int>);
+    std::cout << "Sorted: " << b4.elapsed() / 1000000.0 << "\n";
+    selectionSort(v3, ascending<int>);
+    std::cout << "Reverse Sorted: " << b4.elapsed() / 1000000.0 << "\n"; 
+
 }
